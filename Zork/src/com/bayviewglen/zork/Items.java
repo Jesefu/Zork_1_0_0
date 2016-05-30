a273f1198d0d385421357128a91a34a1225305d3
jfu <jfu@jfu-h.bayviewglen.ca>
2016-05-30 13:55:57 -0400

    Inventory Modifications

--- Zork/src/com/bayviewglen/zork/Items.java
+++ Zork/src/com/bayviewglen/zork/Items.java
@@ -1,10 +1,11 @@
+
 package com.bayviewglen.zork;
 
 public class Items {
 	private String name;
 	private double weight;
 	
-	public void Item(String n, double w) {
+	public Items(String n, double w) {
 		this.name = n;
 		this.weight = w; 
 	}
---
+++ Zork/src/com/bayviewglen/zork/RoomInventory.java
@@ -0,0 +1,9 @@
+package com.bayviewglen.zork;
+
+import java.util.ArrayList;
+
+public class RoomInventory {
+	private ArrayList<Items> roomItems = new ArrayList<Items>();
+	private ArrayList<String> characters = new ArrayList<String>(); 
+	
+}
